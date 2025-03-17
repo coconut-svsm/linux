4e7927c0eb1fa2969d4fa2964ffa9eabbd970674
Nikunj A Dadhania <nikunj@amd.com>
2025-03-17 10:53:05 +0530

    x86/cpufeatures: Add SNP Secure TSC
    The Secure TSC feature for SEV-SNP allows guests to securely use the RDTSC
    and RDTSCP instructions, ensuring that the parameters used cannot be
    altered by the hypervisor once the guest is launched. For more details,
    refer to the AMD64 APM Vol 2, Section "Secure TSC".
    
    Acked-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>
    Tested-by: Vaishali Thakkar <vaishali.thakkar@suse.com>
    Signed-off-by: Nikunj A Dadhania <nikunj@amd.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -442,8 +442,9 @@
 #define X86_FEATURE_SME			(19*32+ 0) /* "sme" AMD Secure Memory Encryption */
 #define X86_FEATURE_SEV			(19*32+ 1) /* "sev" AMD Secure Encrypted Virtualization */
 #define X86_FEATURE_VM_PAGE_FLUSH	(19*32+ 2) /* VM Page Flush MSR is supported */
-#define X86_FEATURE_SEV_ES		(19*32+ 3) /* "sev_es" AMD Secure Encrypted Virtualization - Encrypted State */
-#define X86_FEATURE_SEV_SNP		(19*32+ 4) /* "sev_snp" AMD Secure Encrypted Virtualization - Secure Nested Paging */
+#define X86_FEATURE_SEV_ES		(19*32+ 3) /* "sev_es" Secure Encrypted Virtualization - Encrypted State */
+#define X86_FEATURE_SEV_SNP		(19*32+ 4) /* "sev_snp" Secure Encrypted Virtualization - Secure Nested Paging */
+#define X86_FEATURE_SNP_SECURE_TSC	(19*32+ 8) /* SEV-SNP Secure TSC */
 #define X86_FEATURE_V_TSC_AUX		(19*32+ 9) /* Virtual TSC_AUX */
 #define X86_FEATURE_SME_COHERENT	(19*32+10) /* AMD hardware-enforced cache coherency */
 #define X86_FEATURE_RESTRICTED_INJECTION	(19*32+12) /* AMD SEV Restricted Injection */
